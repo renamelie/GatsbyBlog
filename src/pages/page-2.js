609029eae0571006b1e1a67154067268a05de2fe
Amelie Ren <meilou@hotmail.fr>
2020-08-07 16:02:09 +0200

    fix

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Header from '../components/Header'
+import Header from '../components/header'
 import SEO from '../components/seo'
 
 const SecondPage = () => (
